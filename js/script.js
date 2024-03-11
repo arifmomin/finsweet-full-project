5782a0a7ae228aa784bc92a777ee4e704a15f527
Arif Momin <148381072+arifmomin@users.noreply.github.com>
2024-03-12 02:29:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.client_slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay:true,
+    prevArrow:false,
+    nextArrow:false
+  });
